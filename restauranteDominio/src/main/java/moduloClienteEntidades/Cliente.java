4060f97ffadc71f60da2550c4ad1e6866b06a09b
Isaac <isaacgro80@gmail.com>
2025-03-31 18:08:49 -0700

    creacion de Cliente y ClienteFrecuente
    se crearon ahorita se van a crear las demas entidades

---
+++ restauranteDominio/src/main/java/moduloClienteEntidades/Cliente.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package moduloClienteEntidades;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author isaac
+ */
+@Entity
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+  
+   
+   
+
+    
+}
